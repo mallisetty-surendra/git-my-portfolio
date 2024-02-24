7f1c0e3d5ae7cbad43d4423d5ce31852466b1164
surendra4449 <malisettysurendra@gmail.com>
2024-02-24 18:19:51 +0530

    version 1.0

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        curs: ['Cedarville Cursive'],
+        sans:['Roboto', 'sans-serif']
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
